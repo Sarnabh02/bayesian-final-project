3530044eb06c9d8c6a9f3b0b60ff5d09713ab88f
Alyssa Schechtel <123844275+alyssaschechtelUIUC@users.noreply.github.com>
2023-12-03 10:27:31 -0600

    file rename, DIC, model 2
    Renamed data file to healthdata.xlsx for easier reading into R, added sectioning to the R script, added DIC checks and an extra model with no g's

--- Final Project.R
+++ Final Project.R
@@ -1,6 +1,6 @@
 library(readxl)
-hdata <- read_xlsx("healthdata.xlsx", sheet =2)
-hdefs <- read_xlsx("healthdata.xlsx", sheet =1)
+hdata <- read_xlsx("data/healthdata.xlsx", sheet =2)
+hdefs <- read_xlsx("data/healthdata.xlsx", sheet =1)
 
 
 
@@ -15,6 +15,10 @@ sig2hat<-summary(fit)$sigma^2
 p<- ncol(Xmat)-1
 
 library(rjags)
+
+
+# Model 1 : Mixed g's prior -----------------------------------------------
+
 
 cat('
 data {
@@ -74,4 +78,72 @@ gelman.diag(x, autoburnin=FALSE, multivariate=FALSE)
 
 summary(x)
 
+### Replication and DIC check
+
+update(m, 1000)
+dic.samples(m, 10000, type="pD")
+
+# Model 2: No g  ----------------------------------------------------------
+
+cat('
+data {
+  dim.X <- dim(X)
+  n <- dim.X[1]
+}
+
+model {
+  for(i in 1:n) {
+    y[i] ~ dnorm(mu[i], tausq)
+    mu[i] <- alpha + X[i,]%*%beta[]
+  }
+  tausq ~ dgamma(nu0/2,nu0*sig20/2)
+  sigma2 <- 1/tausq
+
+  alpha ~ dnorm(0,.0001)
+  beta ~ dmnorm(B0,tausq*Sig0)
+}
+',file = {final.proj.model1 = tempfile()})
+
+d <- list(y = hdata1$HO18_LifeExpect,
+          X = Xmat[,-1],
+          B0 = rep(0,p),
+          Sig0 = XtX[-1,-1],
+          nu0 =1,
+          sig20=sig2hat
+)
+
+
+inits <- list(list(tausq=1, alpha = 1, beta=rnorm(p) ),
+              list(tausq=1, alpha=-1, beta=rnorm(p,sd=3) ),
+              list(tausq=1, alpha=0, beta=rnorm(p,sd=.5) )
+)
+
+m <- jags.model(final.proj.model1, d, inits, n.chains=3)
+
+### Make a preliminary run of 1000 iterations, with monitoring
+
+x <- coda.samples(m, c("alpha","beta","sigma2"), n.iter=1000)
+
+
+### Assess convergence
+
+gelman.diag(x, autoburnin=FALSE, multivariate=FALSE)
+
+
+### Run 10000 more iterations
+
+x <- coda.samples(m, c("alpha","beta","sigma2"), n.iter=10000)
+
+### Assess convergence
+
+gelman.diag(x, autoburnin=FALSE, multivariate=FALSE)
+
+### Check stats after burn-in
+
+summary(x)
+
+### Replication and DIC check
+
+update(m, 1000)
+dic.samples(m, 10000, type="pD")
 
